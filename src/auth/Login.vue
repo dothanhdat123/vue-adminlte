ccbe76ca82ae56e6f873b23e058c16e322ebad57
van.nguyen <KaUhbeK4cMf264NSPVKbP3DxWbVEeNTt>
2020-04-09 09:41:28 +0700

    update

---
+++ src/auth/Login.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="app flex-row align-items-center">
+    <div class="container">
+      <b-row class="justify-content-center">
+        <b-col md="6">
+          <b-card-group>
+            <b-card no-body class="p-4">
+              <b-card-body>
+                <b-form @submit="onSubmit">
+                  <h1>Login</h1>
+                  <p class="text-muted" style="color: red !important;" v-if="error">{{error}}</p>
+                  <b-input-group class="mb-3">
+                    <b-input-group-prepend><b-input-group-text><i class="icon-user"></i></b-input-group-text></b-input-group-prepend>
+                    <b-form-input type="text" v-model="email" class="form-control" placeholder="Username" autocomplete="username email" />
+                  </b-input-group>
+                  <b-input-group class="mb-4">
+                    <b-input-group-prepend><b-input-group-text><i class="icon-lock"></i></b-input-group-text></b-input-group-prepend>
+                    <b-form-input type="password" v-model="password" class="form-control" placeholder="Password" autocomplete="current-password" />
+                  </b-input-group>
+                  <b-row>
+                    <b-col cols="6">
+                      <b-button variant="primary" type="submit" class="px-4">Login</b-button>
+                    </b-col>
+                  </b-row>
+                </b-form>
+              </b-card-body>
+            </b-card>
+          </b-card-group>
+        </b-col>
+      </b-row>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        email: '',
+        password: '',
+        error: null
+      }
+    },
+    methods: {
+      onSubmit (e) {
+        e.preventDefault()
+        window.location.href = '/dashboard'
+      }
+    }
+  }
+</script>

061aa37b7dfed6d7ae5e86ae2aac20d8eb703ae8
van.nguyen <KaUhbeK4cMf264NSPVKbP3DxWbVEeNTt>
2020-04-10 17:12:13 +0700

    update

---
+++ src/components/hightChart/game_statistics.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="overflow-auto">
+    <b-table
+      id="my-table"
+      :items="items"
+      :per-page="perPage"
+      :current-page="currentPage"
+      small
+    ></b-table>
+  </div>
+</template>
+<script>
+  export default {
+    data () {
+      return {
+        items: [
+          { id: 1, first_name: 'Fred', last_name: 'Flintstone' },
+          { id: 2, first_name: 'Wilma', last_name: 'Flintstone' },
+          { id: 3, first_name: 'Barney', last_name: 'Rubble' },
+          { id: 4, first_name: 'Betty', last_name: 'Rubble' },
+          { id: 5, first_name: 'Pebbles', last_name: 'Flintstone' },
+          { id: 6, first_name: 'Bamm Bamm', last_name: 'Rubble' },
+          { id: 7, first_name: 'The Great', last_name: 'Gazzoo' },
+          { id: 8, first_name: 'Rockhead', last_name: 'Slate' },
+          { id: 9, first_name: 'Pearl', last_name: 'Slaghoople' },
+          { id: 10, first_name: 'Fred', last_name: 'Flintstone' },
+          { id: 11, first_name: 'Wilma', last_name: 'Flintstone' },
+          { id: 12, first_name: 'Barney', last_name: 'Rubble' },
+          { id: 13, first_name: 'Betty', last_name: 'Rubble' },
+          { id: 14, first_name: 'Pebbles', last_name: 'Flintstone' },
+          { id: 15, first_name: 'Bamm Bamm', last_name: 'Rubble' },
+          { id: 16, first_name: 'The Great', last_name: 'Gazzoo' },
+          { id: 17, first_name: 'Rockhead', last_name: 'Slate' },
+          { id: 18, first_name: 'Pearl', last_name: 'Slaghoople' },
+          { id: 19, first_name: 'Rockhead', last_name: 'Slate' },
+          { id: 10, first_name: 'Pearl', last_name: 'Slaghoople' }
+        ]
+      }
+    },
+    computed: {
+      rows () {
+        return this.items.length
+      }
+    }
+  }
+</script>
---
+++ src/components/hightChart/revenue_20day.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <h1>Biểu đồ user theo type</h1>
+    <va-chart :chart-config='lineChartConfig'></va-chart>
+  </div>
+</template>
+
+<script>
+  import VAChart from '../VAChart'
+  export default {
+    components: {
+      'va-chart': VAChart
+    },
+    data () {
+      return {
+        lineChartConfig: {
+          type: 'line',
+          data: {
+            datasets: [{
+              data: []
+            }],
+            labels: []
+          }
+        }
+      }
+    },
+    methods: {
+      getdata () {
+        let number = [10, 60, 30, 40, 50, 30, 10, 60, 30, 40, 50, 30, 10, 60, 30, 40, 50, 30, 50, 30]
+        let lable = ['day 1', 'day 2', 'day 3', 'day 4', 'day 5', 'day 6', 'day 7', 'day 8', 'day 9', 'day 10', 'day 11', 'day 12', 'day 13', 'day 14', 'day 15', 'day 16', 'day 17', 'day 18', 'day 19', 'day 20']
+        this.lineChartConfig.data.datasets[0].data = number
+        this.lineChartConfig.data.labels = lable
+      }
+    },
+    created () {
+      this.getdata()
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
